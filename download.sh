c7f88a60852d22ec0a647a84be5df852eb56dceb
USER <user@AL01431835.local>
2019-03-23 15:34:56 +0900

    Update download.sh and install.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-zip_file=obstacletower_v1.2_linux.zip
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  zip_file=obstacletower_v1.2_linux.zip
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  zip_file=obstacletower_v1.2_osx.zip
+fi
+
 wget --verbose https://storage.googleapis.com/obstacle-tower-build/v1.2/$zip_file -O submodules/obstacle-tower-challenge/$zip_file
 cd submodules/obstacle-tower-challenge
 unzip $zip_file
--- install.sh
+++ install.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
+base=0
+
+for arg
+do
+  case $arg in
+    -base) base=1;;
+    *) echo "Usage: `basename $0` [-base]" 1>&2
+       exit 1;;
+  esac
+done
+
 echo "Get modules - (obstacle-tower-env, obstacle-tower-challenge)"
 git submodule init
 git submodule update
 
 cd submodules/obstacle-tower-env
 echo "Installing [obstacle-tower-env]"
-pip3 install -e .
+pip install -e .
 cd ../../
 
 cd submodules/obstacle-tower-challenge
 echo "Installing [obstacle-tower-challenge]"
-pip3 install -r requirements.txt
-pip3 install matplotlib
+pip install -r requirements.txt
+pip install matplotlib
 cd ../../
 
-cd submodules/baselines
-echo "Installing [openai/baselines]"
-sudo apt-get update
-sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
-pip3 install tensorflow-gpu==1.8.0
-pip3 install -e .
-cd ../../
-
+if [ ${base} -eq 1 ];then
+  cd submodules/baselines
+  echo "Installing [openai/baselines]"
+  sudo apt-get update
+  sudo apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
+  pip3 install tensorflow-gpu==1.8.0
+  pip3 install -e .
+  cd ../../
+fi
 
 echo "Downloading obstacle tower environment"
 . ./download.sh
